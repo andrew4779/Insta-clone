04c9f16dee7527ffdb8b1cf793095855c2ddfabf
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-03-31 22:53:17 +0300

    templates folder initial commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import *
 
-# Create your views here.
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+# Create your views here
+
