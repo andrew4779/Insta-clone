c1991460f11d6e2660b79f0806671a64d0f58fb7
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-03-31 23:05:21 +0300

    initial commit

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from  django.urls import path
 from . import views
 from django.conf import settings
@@ -6,5 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/', views.profile, name= 'profile'),
     
 ]
--- app/views.py
+++ app/views.py
@@ -7,3 +7,8 @@ import cloudinary.uploader
 import cloudinary.api
 # Create your views here
 
+@login_required(login_url='/accounts/login/')
+def index(request):
+    # get all the images from the database and order them by the date they were created
+    images = Image.objects.all().order_by('-image_date')
+    return render(request, 'index.html', {'images': images})
