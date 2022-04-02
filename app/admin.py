f38523fb84e8bdab2b220f4b28434bd197f36ae9
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-02 10:28:23 +0300

    made final changes

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
-# from .models import Image, Profile, Likes, Comments
+from django.contrib import admin
+from .models import Image, Profile, Likes, Comments
 
-# # Register your models here.
-# admin.site.register(Image)
-# admin.site.register(Profile)
-# admin.site.register(Likes)
-# admin.site.register(Comments)
+# Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Likes)
+admin.site.register(Comments)
