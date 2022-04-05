02340204a08362fa5acfa52ea83813157b623630
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-05 11:31:42 +0300

    test database

--- app/tests.py
+++ app/tests.py
@@ -1,1 +1,28 @@
 from django.test import TestCase
+
+# Create your tests here.
+from .models import *
+
+# test image class
+
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        # create a user
+        user = User.objects.create(
+            username='test_user',
+            first_name='Andrew',
+            last_name='Kimani'
+        )
+        Image.objects.create(
+            image_name='test_image',
+            image_caption='test image',
+            profile_id=user.id,
+            user_id=user.id
+        )
+
+    def test_image_name(self):
+        image = Image.objects.get(image_name='test_image')
+        self.assertEqual(image.image_name, 'test_image')
+
+
